4a121f13b143b295cab90467f3b3bed8f495597e
Michael Dominic Bejar <dominic@aiming-inc.com.ph>
2017-09-25 18:21:04 +0800

    about added

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,31 @@ import {
   Switch
 } from 'react-router-dom';
 
+const divStyle ={
+    color : '#fff',
+    fontStyle : 'arial,tahoma,sans-serif',
+    fontSize : 20
+}
+
+
+let cars = ['honda','mistubishi','nissan']
+let myCars = new Set(cars);
+
+
+myCars.add('mercedes');
+myCars.add({"bike":"harley"});
+
+function showCars($cars = 'no cars'){
+    console.log($cars)
+}
+
 
 class About extends Component {
-   
     
-    render(){
+     render(){
+        showCars(myCars);
         return(
-            <div>
+            <div style={divStyle}>
                 <h1>About wrewrww</h1>
             </div>
         );
